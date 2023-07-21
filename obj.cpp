938e2c39ef6091321058a52cad740d1048461a9d
dieggspapu <alo20172@uvg.edu.gt>
2023-07-21 11:08:41 -0600

    feat:
    obj. gotten is  a dog

---
+++ obj.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <threads.h>
+#include <list>
+#include "mathLibrary.h"
+using namespace std;
